b4a94b64782008ac63b72f506ca2586016aa2f07
Gerson Castillo <gerscastillo1@gmail.com>
2025-12-21 22:45:59 -0500

    Fix formatNumber undefined

--- client/src/pages/CalculateRatePage.js
+++ client/src/pages/CalculateRatePage.js
@@ -209,6 +209,11 @@ export default function CalculateRatePage({ embedded, initialValues, prefill, on
     }
   }
 
+  function formatNumber(value) {
+    if (typeof value !== 'number' || Number.isNaN(value)) return '-';
+    return new Intl.NumberFormat('en-US', { maximumFractionDigits: 2 }).format(value);
+  }
+
   function resetContactForm() {
     setContactName('');
     setContactEmail('');
